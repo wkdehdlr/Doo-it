8382e63ba82cad92bb8f54140c60f6afbb1bfb14
wkdehdlr <wkdehdlr@gmail.com>
2021-01-29 21:41:00 +0900

    Update 조합.cpp

--- Algorithm/cheat/cheat/조합.cpp
+++ Algorithm/cheat/cheat/조합.cpp
@@ -5,7 +5,8 @@ using namespace std;
 bool visited[8];
 int arr[8];
 int N, M;
-
+//nCm
+//5C2
 void func(int c, int k) {
 	if (c == M) {
 		for (int i = 0; i < M; ++i) {
@@ -17,8 +18,10 @@ void func(int c, int k) {
 
 	for (int i = k; i < N; ++i) {
 		if (!visited[i]) {
-			arr[c] = i + 1;
+			visited[i]=true;
+			arr[c] = i;
 			func(c + 1, i + 1);
+			visited[i]=false;
 		}
 	}
 }
